4c768766e885554646ffa2cebc415a35e31b0e1c
Manorkie25 <manorkie96@gmail.com>
2025-07-07 12:20:48 +0000

    login and register pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,54 @@
+import { Link } from "react-router";
+import blood from "../assets/Images/blood.png";
+
 
 export default function Login() {
   
+
   return (
     <>
-  <h1 className="text-2xl text text-black">Welcome to LifeLine</h1>
+    <div className="min-h-screen flex  items-start md:items-start  justify-center bg-gradient-to-b from-gray-300 to-red-50">
+      <div className="bg-white  shadow-2xl  h-130 rounded-lg p-4  sm:p-4 w-full max-w-sm sm:max-w-md md:max-w-lg text-center mt-4  md:mt-10 ">
+      
+        <div className="flex justify-center mb-4 space-x-2">
+          <span className="text-red-500 text-2xl"><img src={blood} alt="blood" className="w-7" /></span>
+          <span className="text-buttonblue text-2xl">🏥</span>
+        </div>
+        <div>
+        <h2 className="text-2xl font-bold mb-6">Blood Bank Staff Login</h2>
+       </div>
+    
+        <form className="space-y-4 ">
+          <p className=" flex justify-items-start font-bold mb-0" >Email</p>
+          <input
+            type="email"
+            placeholder="Enter Your Email"
+            className="w-full px-4 py-2    border border-gray-400 rounded focus:outline-none focus:ring-2 focus:ring-blue-300"
+          />
+
+          <p className=" flex justify-items-start font-bold mb-0">Password</p>
+          <input
+            type="password"
+            placeholder="Password"
+            className="w-full px-4 py-2 border border-gray-400 rounded focus:outline-none focus:ring-2 focus:ring-blue-300"
+          />
+          <button
+            type="submit"
+            className="w-full py-2  mt-5 bg-buttonred text-white rounded hover:bg-red-700 transition">Login</button>
+        </form>
+
+        <div className="mt-6 text-sm">
+          <p className="text-buttonblue">Don't have an account?{" "}
+            <Link to="/register" className="text-buttonblue  hover:underline">Register  </Link>
+          </p>
+          <p className="mt-8">
+            <Link to="/" className="text-gray-600 hover:underline">
+              Back to Home
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,74 @@
+import { Link } from "react-router";
+import blood from "../assets/Images/blood.png";
 
 export default function Register() {
-  
   return (
     <>
-  <h1 className="text-2xl text text-black">Welcome to LifeLine</h1>
+      <div className="min-h-screen flex items-start md:items-start justify-center bg-gradient-to-b from-gray-300 to-red-50 px-2 sm:px-4 ">
+        <div className="bg-white shadow-2xl rounded-lg sm:p-4 w-full max-w-sm sm:max-w-md md:max-w-lg text-center  md:mt-5 ">
+          <div className="flex justify-center mb-4 space-x-2">
+            <span className="text-red-500 text-2xl"><img src={blood} alt="blood" className="w-7" /></span>
+            <span className="text-buttonblue text-2xl">🏥</span>
+          </div>
+          <div>
+            <h2 className="text-xl sm:text-2xl font-bold mb-4 sm:mb-6">Blood Bank Staff Registration</h2>
+          </div>
+
+          <form className="space-y-3 sm:space-y-4 text-left">
+            <p className="font-bold mb-0">Hospital Name</p>
+            <input
+              type="text"
+              placeholder="Enter Your Hospital Name"
+              className="w-full px-3 py-2 sm:px-4 sm:py-2 border border-gray-400 rounded focus:outline-none focus:ring-2 focus:ring-blue-300"
+            />
+            <p className="font-bold mb-0">Location</p>
+            <input
+              type="text"
+              placeholder="Your Location"
+              className="w-full px-3 py-2 sm:px-4 sm:py-2 border border-gray-400 rounded focus:outline-none focus:ring-2 focus:ring-blue-300"
+            />
+            <p className="font-bold mb-0">Contact Number</p>
+            <input
+              type="text"
+              placeholder="Enter Your Contact Number"
+              className="w-full px-3 py-2 sm:px-4 sm:py-2 border border-gray-400 rounded focus:outline-none focus:ring-2 focus:ring-blue-300"
+            />
+            <p className="font-bold mb-0">Email</p>
+            <input
+              type="email"
+              placeholder="Enter Your Email"
+              className="w-full px-3 py-2 sm:px-4 sm:py-2 border border-gray-400 rounded focus:outline-none focus:ring-2 focus:ring-blue-300"
+            />
+            <p className="font-bold mb-0">Password</p>
+            <input
+              type="password"
+              placeholder="Set Your Password"
+              className="w-full px-3 py-2 sm:px-4 sm:py-2 border border-gray-400 rounded focus:outline-none focus:ring-2 focus:ring-blue-300"
+            />
+
+            <button
+              type="submit"
+              className="w-full py-2 mt-4 sm:mt-5 bg-buttonred text-white rounded hover:bg-red-700 transition"
+            >
+              Register
+            </button>
+          </form>
+
+          <div className="mt-5 sm:mt-6 text-sm">
+            <p className="text-buttonblue">
+              Already have an account?{" "}
+              <Link to="/login" className="text-buttonblue hover:underline">
+                Login
+              </Link>
+            </p>
+            <p className="mt-6">
+              <Link to="/" className="text-gray-600 hover:underline">
+                Back to Home
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
